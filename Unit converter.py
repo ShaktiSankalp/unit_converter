6217cd2c743b059ccf437dccf70d28540f6d79d4
Kshitij Singh <119831348+thisiskshitij@users.noreply.github.com>
2022-12-06 19:57:25 +0530

    Add files via upload

---
+++ Unit converter.py
@@ -0,0 +1,16 @@
+a = int(input("Enter value in KM: " ))
+b = int(a * 1000)
+
+print("Conversion to M:",b )
+
+a = int(input("Enter value in KG: " ))
+b = int(a * 1000)
+
+print("Conversion to GM:",b )
+
+a = int(input("Enter temperature in Celcius: ", ))
+b = int((a * 9/5 )+ 32)
+
+print("Conversion to fahrenheit:",b )
+
+
